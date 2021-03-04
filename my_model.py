9b0daa93159f592a2ab5dc3914f39864d7291b0c
Aastha jain <50754325+Aasthajain123@users.noreply.github.com>
2021-03-04 12:32:31 +0530

    Update my_model.py

--- my_model.py
+++ my_model.py
@@ -1,7 +1,7 @@
 import pandas as pd
 //dataset = pd.read_csv('SalaryData.csv')
 y = dataset['Salary']
-x = dataset['YearsExperience']
+//x = dataset['YearsExperience']
 X = x.values.reshape(30,1)
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=42)
